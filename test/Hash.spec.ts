3d15925bb6faa32c64aaf878bcbbc319049262d3
George Theofilis <theofilis.g@gmail.com>
2017-11-14 16:49:46 +0200

    Update Hash tests

--- test/Hash.spec.ts
+++ test/Hash.spec.ts
@@ -2,11 +2,11 @@ import Hash from '../lib/index';
 
 import { expect } from 'chai';
 
-describe(`Check hash function`, () => {
-  it('Create a new tree (Even number of items)', () => {
-    console.log(Hash('md2', 'testing'));
-
-    console.log('\nNext should be:\n0c1e02ff');
-    console.log(Hash('adler32', 'testing'));
+describe(`Check hash libraries`, () => {
+  it('MD5 tests', () => {
+    expect(Hash.hash('md2', 'testing')).to.equal('fc134df10d6ecafceb5c75861d01b41f');
+  });
+  it('adler32 tests', () => {
+    expect(Hash.hash('adler32', 'testing')).to.equal('0c1e02ff');
   });
 });
