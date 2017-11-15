84712da76ff0575a4011f58ee706491a3d3b326b
George Theofilis <theofilis.g@gmail.com>
2017-11-15 14:00:37 +0200

    Add bench for merkle tree creation

--- lib/Tree.ts
+++ lib/Tree.ts
@@ -1,20 +1,20 @@
-import * as CryptoJS from "crypto-js";
+import * as CryptoJS from 'crypto-js';
 
 export class MerkleTree {
   private _root: string;
 
-  get root(): string {
+  get root (): string {
     return this._root;
   }
 
-  constructor(
+  constructor (
     private _items: Array<string>,
     private _hashFun: Function = CryptoJS.SHA256
   ) {
-    this._root = "";
+    this._root = '';
   }
 
-  create() {
+  create () {
     const items = this.clone(this._items);
 
     let level = this.calculate(items);
@@ -25,19 +25,19 @@ export class MerkleTree {
     this._root = level[0];
   }
 
-  private calculate(items: Array<string>) {
+  private calculate (items: Array<string>) {
     const tempItems: Array<string> = [];
 
-    for (var index = 0; index < items.length; index = index + 2) {
+    for (let index = 0; index < items.length; index = index + 2) {
       let left = items[index];
-      let right = items[index + 1] || "";
+      let right = items[index + 1] || '';
       tempItems.push(this._hashFun(left + right).toString());
     }
 
     return tempItems;
   }
 
-  private clone(items: Array<string>) {
+  private clone (items: Array<string>) {
     const tempItems: Array<string> = [];
 
     items.forEach(item => {
